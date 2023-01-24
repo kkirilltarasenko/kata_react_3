78240e845e1aeec784720f7063844180bbc623b0
kkirilltarasenko <75016013+kkirilltarasenko@users.noreply.github.com>
2023-01-24 17:59:47 +0300

    Started work with request

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.scss';
 /* Components */
 import Logo from '../Logo/Logo';
 import Tabs from '../Tabs/Tabs';
 import Filter from '../Filter/Filter';
 
+import { Ticket } from '../test';
+
 function App(): JSX.Element {
+  useEffect(() => {
+    async function fetchTickets(): Promise<{ tickets: Ticket[]; stop: boolean }> {
+      const searchID: { searchId: string } = await fetch(
+        'https://aviasales-test-api.kata.academy/search'
+      ).then(async (res) => await res.json());
+      return await fetch(
+        `https://aviasales-test-api.kata.academy/tickets?searchId=${searchID.searchId}`
+      ).then(async (res) => await res.json());
+    }
+
+    console.log(fetchTickets());
+  });
+
   return (
     <div className="App">
       <Logo marginTop={50} marginBottom={50} />
